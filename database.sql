88f7138da5db9eded03144f884161ee51fe946c5
Jean Thomassen <codeaconjean@gmail.com>
2024-09-30 18:15:54 -0300

    Correccion

--- database.sql
+++ database.sql
@@ -1,22 +1,25 @@
-create table temporadas (
-  id bigint primary key generated always as identity,
-  numero int not null,
-  titulo text
+CREATE TABLE temporadas (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  numero INT NOT NULL,
+  titulo TEXT
 );
 
-create table capitulos (
-  id bigint primary key generated always as identity,
-  titulo text not null,
-  temporada_id bigint references temporadas (id)
+CREATE TABLE capitulos (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  titulo TEXT NOT NULL,
+  temporada_id BIGINT,
+  FOREIGN KEY (temporada_id) REFERENCES temporadas(id)
 );
 
-create table personajes (
-  id bigint primary key generated always as identity,
-  nombre text not null
+CREATE TABLE personajes (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  nombre TEXT NOT NULL
 );
 
-create table personajesporcapitulo (
-  id bigint primary key generated always as identity,
-  capitulo_id bigint references capitulos (id),
-  personaje_id bigint references personajes (id)
+CREATE TABLE personajesporcapitulo (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  capitulo_id BIGINT,
+  personaje_id BIGINT,
+  FOREIGN KEY (capitulo_id) REFERENCES capitulos(id),
+  FOREIGN KEY (personaje_id) REFERENCES personajes(id)
 );
